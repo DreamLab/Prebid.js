37d56402851d178660994ec28a7de8857c86438d
Aleksander Dudek <aleksander.dudek@ringieraxelspringer.pl>
2025-01-15 11:29:13 +0100

    fix missing ext

--- modules/dasBidAdapter.js
+++ modules/dasBidAdapter.js
@@ -193,7 +193,7 @@ function interpretResponse(serverResponse) {
         creativeId: bid.crid || bid.id,
         netRevenue: true,
         dealId: bid.dealid || null,
-        actgMatch: bid.ext.actgMatch,
+        actgMatch: bid.ext?.actgMatch,
         ttl: 300,
         meta: {
           advertiserDomains: bid.adomain || [],
